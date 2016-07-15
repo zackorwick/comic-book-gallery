c8a318aa12a696ddb271649cab72ed190ef1e0b1
zackorwick <zackorwick@live.com>
2016-07-14 23:00:33 -0400

    added detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,12 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller";
+
+        }
+
+
     }
 }
